4d82b64b99e5894b496c4a72f126cc7fb9aab246
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-07-29 16:45:56 +0300

    Initial commit

---
+++ src/main/java/com/example/springcloudhistory/SpringCloudHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudHistoryApplication.class, args);
+	}
+
+}
